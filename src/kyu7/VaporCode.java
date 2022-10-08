c9352a24100aa2487175bc3fde361fb477cb8b7d
Oleg Terentev <terentjev.dr@yandex.ru>
2022-10-08 22:40:48 +0300

    Codewars

--- src/kyu7/VaporCode.java
+++ src/kyu7/VaporCode.java
@@ -15,7 +15,6 @@ package kyu7;
  * Examples:
  *      "Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
  *      "Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
- *
 */
 
 public class VaporCode {
