5acae209eb62d1e809cef92f6f13009f487b3db8
Oleg Terentev <terentjev.dr@yandex.ru>
2022-10-10 22:26:31 +0300

    Codewars

---
+++ src/kyu7/Maskify.java
@@ -0,0 +1,33 @@
+package kyu7;
+
+/*
+ * 7 kyu - Credit Card Mask
+ *
+ * https://www.codewars.com/kata/5412509bd436bd33920011bc/train/java
+ *
+ * DESCRIPTION:
+ * Usually when you buy something, you're asked whether your credit card number,
+ * phone number or answer to your most secret question is still correct.
+ * However, since someone could look over your shoulder, you don't want that shown on your screen.
+ * Instead, we mask it.
+ * Your task is to write a function maskify, which changes all but the last four characters into '#'.
+ *
+ * Examples:
+ *      "4556364607935616" --> "############5616"
+ *      "64607935616" -->      "#######5616"
+ *      "1" -->                "1"
+ *      "" -->                 ""
+ *      // "What was the name of your first pet?"
+ *      "Skippy" --> "##ippy"
+ *      "Nananananananananananananananana Batman!" --> "####################################man!"
+*/
+
+public class Maskify {
+    public static String maskify(String str) {
+        char[] strChars = str.toCharArray();
+        for (int i = 0; i < strChars.length - 4; i++) {
+            strChars[i] = '#';
+        }
+        return String.valueOf(strChars);
+    }
+}
